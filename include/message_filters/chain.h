0156f9151bd1a8e4ea0496e451788da13f2418b7
Chris Lalancette <clalancette@gmail.com>
2024-06-17 14:34:55 +0000

    Rebase from 'debian/rolling/message_filters'

--- include/message_filters/chain.h
+++ include/message_filters/chain.h
@@ -1,41 +1,36 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2010, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+// Copyright 2010, Willow Garage, Inc. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Willow Garage nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef MESSAGE_FILTERS__CHAIN_H
-#define MESSAGE_FILTERS__CHAIN_H
+#ifndef MESSAGE_FILTERS__CHAIN_H_
+#define MESSAGE_FILTERS__CHAIN_H_
 
 #include <vector>
+#include <memory>
 
 #include "message_filters/simple_filter.h"
 #include "message_filters/pass_through.h"
@@ -62,8 +57,7 @@ public:
   std::shared_ptr<F> getFilter(size_t index) const
   {
     std::shared_ptr<void> filter = getFilterForIndex(index);
-    if (filter)
-    {
+    if (filter) {
       return std::static_pointer_cast<F>(filter);
     }
 
@@ -86,7 +80,7 @@ typedef std::shared_ptr<ChainBase> ChainBasePtr;
  *
  * Example:
 \verbatim
-void myCallback(const MsgConstPtr& msg)
+void myCallback(const MsgConstPtr & msg)
 {
 }
 
@@ -101,7 +95,7 @@ c.registerCallback(myCallback);
 \verbatim
 Chain<Msg> c;
 PassThrough<Msg> p;
-c.addFilter(&p);
+c.addFilter( &p);
 c.registerCallback(myCallback);
 \endverbatim
  *
@@ -124,14 +118,14 @@ public:
    * \brief Constructor with filter.  Calls connectInput(f)
    */
   template<typename F>
-  Chain(F& f)
+  explicit Chain(F & f)
   {
     connectInput(f);
   }
 
   struct NullDeleter
   {
-    void operator()(void const*) const
+    void operator()(void const *) const
     {
     }
   };
@@ -140,7 +134,7 @@ public:
    * \brief Add a filter to this chain, by bare pointer.  Returns the index of that filter in the chain.
    */
   template<class F>
-  size_t addFilter(F* filter)
+  size_t addFilter(F * filter)
   {
     std::shared_ptr<F> ptr(filter, NullDeleter());
     return addFilter(ptr);
@@ -150,18 +144,20 @@ public:
    * \brief Add a filter to this chain, by shared_ptr.  Returns the index of that filter in the chain
    */
   template<class F>
-  size_t addFilter(const std::shared_ptr<F>& filter)
+  size_t addFilter(const std::shared_ptr<F> & filter)
   {
     FilterInfo info;
-    info.add_func = std::bind((void(F::*)(const EventType&))&F::add, filter.get(), std::placeholders::_1);
+    info.add_func = std::bind(
+      (void (F::*)(const EventType &)) & F::add, filter.get(), std::placeholders::_1);
     info.filter = filter;
-    info.passthrough = std::make_shared<PassThrough<M> >();
+    info.passthrough = std::make_shared<PassThrough<M>>();
 
     last_filter_connection_.disconnect();
     info.passthrough->connectInput(*filter);
-    last_filter_connection_ = info.passthrough->registerCallback(typename SimpleFilter<M>::EventCallback(std::bind(&Chain::lastFilterCB, this, std::placeholders::_1)));
-    if (!filters_.empty())
-    {
+    last_filter_connection_ = info.passthrough->registerCallback(
+      typename SimpleFilter<M>::EventCallback(
+        std::bind(&Chain::lastFilterCB, this, std::placeholders::_1)));
+    if (!filters_.empty()) {
       filter->connectInput(*filters_.back().passthrough);
     }
 
@@ -180,8 +176,7 @@ public:
   template<typename F>
   std::shared_ptr<F> getFilter(size_t index) const
   {
-    if (index >= filters_.size())
-    {
+    if (index >= filters_.size()) {
       return std::shared_ptr<F>();
     }
 
@@ -192,24 +187,25 @@ public:
    * \brief Connect this filter's input to another filter's output.
    */
   template<class F>
-  void connectInput(F& f)
+  void connectInput(F & f)
   {
     incoming_connection_.disconnect();
-    incoming_connection_ = f.registerCallback(typename SimpleFilter<M>::EventCallback(std::bind(&Chain::incomingCB, this, std::placeholders::_1)));
+    incoming_connection_ = f.registerCallback(
+      typename SimpleFilter<M>::EventCallback(
+        std::bind(&Chain::incomingCB, this, std::placeholders::_1)));
   }
 
   /**
    * \brief Add a message to the start of this chain
    */
-  void add(const MConstPtr& msg)
+  void add(const MConstPtr & msg)
   {
     add(EventType(msg));
   }
 
-  void add(const EventType& evt)
+  void add(const EventType & evt)
   {
-    if (!filters_.empty())
-    {
+    if (!filters_.empty()) {
       filters_[0].add_func(evt);
     }
   }
@@ -217,8 +213,7 @@ public:
 protected:
   virtual std::shared_ptr<void> getFilterForIndex(size_t index) const
   {
-    if (index >= filters_.size())
-    {
+    if (index >= filters_.size()) {
       return std::shared_ptr<void>();
     }
 
@@ -226,21 +221,21 @@ protected:
   }
 
 private:
-  void incomingCB(const EventType& evt)
+  void incomingCB(const EventType & evt)
   {
     add(evt);
   }
 
-  void lastFilterCB(const EventType& evt)
+  void lastFilterCB(const EventType & evt)
   {
     this->signalMessage(evt);
   }
 
   struct FilterInfo
   {
-    std::function<void(const EventType&)> add_func;
+    std::function<void(const EventType &)> add_func;
     std::shared_ptr<void> filter;
-    std::shared_ptr<PassThrough<M> > passthrough;
+    std::shared_ptr<PassThrough<M>> passthrough;
   };
   typedef std::vector<FilterInfo> V_FilterInfo;
 
--- include/message_filters/message_event.h
+++ include/message_filters/message_event.h
@@ -1,30 +1,30 @@
-
-/*
- * Copyright (C) 2010, Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright 2010, Willow Garage, Inc. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Willow Garage nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 // File imported from
 // https://github.com/ros/roscpp_core/blob/38b9663/roscpp_traits/include/ros/message_event.h
@@ -33,7 +33,9 @@
 #define MESSAGE_FILTERS__MESSAGE_EVENT_H_
 
 #include <type_traits>
+#include <map>
 #include <memory>
+#include <string>
 
 #include <rclcpp/rclcpp.hpp>
 
@@ -42,13 +44,11 @@
 // https://github.com/ros2/rcutils/pull/112
 #define RCUTILS_ASSERT assert
 #endif
-// Uncomment below intead
-//#include <rcutils/assert.h>
 
 namespace message_filters
 {
-typedef std::map< std::string, std::string > 	M_string;
-typedef std::shared_ptr< M_string > 	M_stringPtr;
+typedef std::map<std::string, std::string> M_string;
+typedef std::shared_ptr<M_string> M_stringPtr;
 
 template<typename M>
 struct DefaultMessageCreator
@@ -67,7 +67,7 @@ ROS_DEPRECATED inline std::shared_ptr<M> defaultMessageCreateFunction()
 }
 */
 /**
- * \brief Event type for subscriptions, const message_filters::MessageEvent<M const>& can be used in your callback instead of const std::shared_ptr<M const>&
+ * \brief Event type for subscriptions, const message_filters::MessageEvent<M const> & can be used in your callback instead of const std::shared_ptr<M const>&
  *
  * Useful if you need to retrieve meta-data about the message, such as the full connection header, or the publisher's node name
  */
@@ -85,52 +85,59 @@ public:
   : nonconst_need_copy_(true)
   {}
 
-  MessageEvent(const MessageEvent<Message>& rhs)
+  MessageEvent(const MessageEvent<Message> & rhs)
   {
     *this = rhs;
   }
 
-  MessageEvent(const MessageEvent<ConstMessage>& rhs)
+  MessageEvent(const MessageEvent<ConstMessage> & rhs)
   {
     *this = rhs;
   }
 
-  MessageEvent(const MessageEvent<Message>& rhs, bool nonconst_need_copy)
-  {
-    *this = rhs;
-    nonconst_need_copy_ = nonconst_need_copy;
-  }
-
-  MessageEvent(const MessageEvent<ConstMessage>& rhs, bool nonconst_need_copy)
+  MessageEvent(const MessageEvent<Message> & rhs, bool nonconst_need_copy)
   {
     *this = rhs;
     nonconst_need_copy_ = nonconst_need_copy;
   }
 
-  MessageEvent(const MessageEvent<void const>& rhs, const CreateFunction& create)
+  MessageEvent(const MessageEvent<ConstMessage> & rhs, bool nonconst_need_copy)
   {
-    init(std::const_pointer_cast<Message>(std::static_pointer_cast<ConstMessage>(rhs.getMessage())), rhs.getReceiptTime(), rhs.nonConstWillCopy(), create);
+    *this = rhs;
+    nonconst_need_copy_ = nonconst_need_copy;
+  }
+
+  MessageEvent(const MessageEvent<void const> & rhs, const CreateFunction & create)
+  {
+    init(
+      std::const_pointer_cast<Message>(
+        std::static_pointer_cast<ConstMessage>(
+          rhs.getMessage())), rhs.getReceiptTime(), rhs.nonConstWillCopy(), create);
   }
 
   /**
    * \todo Make this explicit in ROS 2.0.  Keep as auto-converting for now to maintain backwards compatibility in some places (message_filters)
    */
-  MessageEvent(const ConstMessagePtr& message)
+  explicit MessageEvent(const ConstMessagePtr & message)
   {
     init(message, rclcpp::Clock().now(), true, message_filters::DefaultMessageCreator<Message>());
   }
 
-  MessageEvent(const ConstMessagePtr& message, rclcpp::Time receipt_time)
+  MessageEvent(const ConstMessagePtr & message, rclcpp::Time receipt_time)
   {
     init(message, receipt_time, true, message_filters::DefaultMessageCreator<Message>());
   }
 
-  MessageEvent(const ConstMessagePtr& message, rclcpp::Time receipt_time, bool nonconst_need_copy, const CreateFunction& create)
+  MessageEvent(
+    const ConstMessagePtr & message, rclcpp::Time receipt_time, bool nonconst_need_copy,
+    const CreateFunction & create)
   {
     init(message, receipt_time, nonconst_need_copy, create);
   }
 
-  void init(const ConstMessagePtr& message, rclcpp::Time receipt_time, bool nonconst_need_copy, const CreateFunction& create)
+  void init(
+    const ConstMessagePtr & message, rclcpp::Time receipt_time, bool nonconst_need_copy,
+    const CreateFunction & create)
   {
     message_ = message;
     receipt_time_ = receipt_time;
@@ -138,15 +145,21 @@ public:
     create_ = create;
   }
 
-  void operator=(const MessageEvent<Message>& rhs)
+  void operator=(const MessageEvent<Message> & rhs)
   {
-    init(std::static_pointer_cast<Message>(rhs.getMessage()), rhs.getReceiptTime(), rhs.nonConstWillCopy(), rhs.getMessageFactory());
+    init(
+      std::static_pointer_cast<Message>(rhs.getMessage()),
+      rhs.getReceiptTime(), rhs.nonConstWillCopy(), rhs.getMessageFactory());
     message_copy_.reset();
   }
 
-  void operator=(const MessageEvent<ConstMessage>& rhs)
+  void operator=(const MessageEvent<ConstMessage> & rhs)
   {
-    init(std::const_pointer_cast<Message>(std::static_pointer_cast<ConstMessage>(rhs.getMessage())), rhs.getReceiptTime(), rhs.nonConstWillCopy(), rhs.getMessageFactory());
+    init(
+      std::const_pointer_cast<Message>(
+        std::static_pointer_cast<ConstMessage>(
+          rhs.getMessage())), rhs.getReceiptTime(), rhs.nonConstWillCopy(),
+      rhs.getMessageFactory());
     message_copy_.reset();
   }
 
@@ -163,54 +176,52 @@ public:
   /**
    * \brief Retrieve a const version of the message
    */
-  const std::shared_ptr<ConstMessage>& getConstMessage() const { return message_; }
+  const std::shared_ptr<ConstMessage> & getConstMessage() const {return message_;}
 
   /**
    * \brief Returns the time at which this message was received
    */
-  rclcpp::Time getReceiptTime() const { return receipt_time_; }
+  rclcpp::Time getReceiptTime() const {return receipt_time_;}
 
-  bool nonConstWillCopy() const { return nonconst_need_copy_; }
-  bool getMessageWillCopy() const { return !std::is_const<M>::value && nonconst_need_copy_; }
+  bool nonConstWillCopy() const {return nonconst_need_copy_;}
+  bool getMessageWillCopy() const {return !std::is_const<M>::value && nonconst_need_copy_;}
 
-  bool operator<(const MessageEvent<M>& rhs)
+  bool operator<(const MessageEvent<M> & rhs)
   {
-    if (message_ != rhs.message_)
-    {
+    if (message_ != rhs.message_) {
       return message_ < rhs.message_;
     }
 
-    if (receipt_time_ != rhs.receipt_time_)
-    {
+    if (receipt_time_ != rhs.receipt_time_) {
       return receipt_time_ < rhs.receipt_time_;
     }
 
     return nonconst_need_copy_ < rhs.nonconst_need_copy_;
   }
 
-  bool operator==(const MessageEvent<M>& rhs)
+  bool operator==(const MessageEvent<M> & rhs)
   {
-    return message_ == rhs.message_ && receipt_time_ == rhs.receipt_time_ && nonconst_need_copy_ == rhs.nonconst_need_copy_;
+    return message_ == rhs.message_ && receipt_time_ == rhs.receipt_time_ &&
+           nonconst_need_copy_ == rhs.nonconst_need_copy_;
   }
 
-  bool operator!=(const MessageEvent<M>& rhs)
+  bool operator!=(const MessageEvent<M> & rhs)
   {
     return !(*this == rhs);
   }
 
-  const CreateFunction& getMessageFactory() const { return create_; }
+  const CreateFunction & getMessageFactory() const {return create_;}
 
 private:
   template<typename M2>
-  typename std::enable_if<!std::is_void<M2>::value, std::shared_ptr<M> >::type copyMessageIfNecessary() const
+  typename std::enable_if<!std::is_void<M2>::value,
+    std::shared_ptr<M>>::type copyMessageIfNecessary() const
   {
-    if (std::is_const<M>::value || !nonconst_need_copy_)
-    {
+    if (std::is_const<M>::value || !nonconst_need_copy_) {
       return std::const_pointer_cast<Message>(message_);
     }
 
-    if (message_copy_)
-    {
+    if (message_copy_) {
       return message_copy_;
     }
 
@@ -222,13 +233,15 @@ private:
   }
 
   template<typename M2>
-  typename std::enable_if<std::is_void<M2>::value, std::shared_ptr<M> >::type copyMessageIfNecessary() const
+  typename std::enable_if<std::is_void<M2>::value,
+    std::shared_ptr<M>>::type copyMessageIfNecessary() const
   {
     return std::const_pointer_cast<Message>(message_);
   }
 
   ConstMessagePtr message_;
-  // Kind of ugly to make this mutable, but it means we can pass a const MessageEvent to a callback and not worry about other things being modified
+  // Kind of ugly to make this mutable, but it means we can pass a const MessageEvent
+  // to a callback and not worry about other things being modified
   mutable MessagePtr message_copy_;
   rclcpp::Time receipt_time_;
   bool nonconst_need_copy_;
@@ -237,7 +250,8 @@ private:
   static const std::string s_unknown_publisher_string_;
 };
 
-template<typename M> const std::string MessageEvent<M>::s_unknown_publisher_string_("unknown_publisher");
+template<typename M> const std::string MessageEvent<M>::s_unknown_publisher_string_(
+  "unknown_publisher");
 
 }  // namespace message_filters
 
