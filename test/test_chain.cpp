6836c317841aca9316c1fb837f94fb2c41c8680d
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-09-17 14:44:22 +0200

    Imported upstream version '4.3.9' of 'upstream'

--- test/test_chain.cpp
+++ test/test_chain.cpp
@@ -1,37 +1,43 @@
-// Copyright 2010, Willow Garage, Inc. All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright
-//      notice, this list of conditions and the following disclaimer.
-//
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//    * Neither the name of the Willow Garage nor the names of its
-//      contributors may be used to endorse or promote products derived from
-//      this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
 
 #include <gtest/gtest.h>
 
-#include <memory>
-
 #include <rclcpp/rclcpp.hpp>
-#include "message_filters/chain.hpp"
+#include "message_filters/chain.h"
+
+using namespace message_filters;
 
 struct Msg
 {
@@ -54,13 +60,13 @@ public:
   int32_t count_;
 };
 
-typedef std::shared_ptr<message_filters::PassThrough<Msg>> PassThroughPtr;
+typedef std::shared_ptr<PassThrough<Msg> > PassThroughPtr;
 
 TEST(Chain, simple)
 {
   Helper h;
-  message_filters::Chain<Msg> c;
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  Chain<Msg> c;
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
   c.registerCallback(std::bind(&Helper::cb, &h));
 
   c.add(std::make_shared<Msg>());
@@ -72,11 +78,11 @@ TEST(Chain, simple)
 TEST(Chain, multipleFilters)
 {
   Helper h;
-  message_filters::Chain<Msg> c;
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  Chain<Msg> c;
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
   c.registerCallback(std::bind(&Helper::cb, &h));
 
   c.add(std::make_shared<Msg>());
@@ -88,16 +94,16 @@ TEST(Chain, multipleFilters)
 TEST(Chain, addingFilters)
 {
   Helper h;
-  message_filters::Chain<Msg> c;
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  Chain<Msg> c;
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
   c.registerCallback(std::bind(&Helper::cb, &h));
 
   c.add(std::make_shared<Msg>());
   EXPECT_EQ(h.count_, 1);
 
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
 
   c.add(std::make_shared<Msg>());
   EXPECT_EQ(h.count_, 2);
@@ -106,11 +112,11 @@ TEST(Chain, addingFilters)
 TEST(Chain, inputFilter)
 {
   Helper h;
-  message_filters::Chain<Msg> c;
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  Chain<Msg> c;
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
   c.registerCallback(std::bind(&Helper::cb, &h));
 
-  message_filters::PassThrough<Msg> p;
+  PassThrough<Msg> p;
   c.connectInput(p);
   p.add(std::make_shared<Msg>());
   EXPECT_EQ(h.count_, 1);
@@ -122,8 +128,8 @@ TEST(Chain, inputFilter)
 TEST(Chain, nonSharedPtrFilter)
 {
   Helper h;
-  message_filters::Chain<Msg> c;
-  message_filters::PassThrough<Msg> p;
+  Chain<Msg> c;
+  PassThrough<Msg> p;
   c.addFilter(&p);
   c.registerCallback(std::bind(&Helper::cb, &h));
 
@@ -135,46 +141,46 @@ TEST(Chain, nonSharedPtrFilter)
 
 TEST(Chain, retrieveFilter)
 {
-  message_filters::Chain<Msg> c;
+  Chain<Msg> c;
 
-  ASSERT_FALSE(c.getFilter<message_filters::PassThrough<Msg>>(0));
+  ASSERT_FALSE(c.getFilter<PassThrough<Msg> >(0));
 
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
 
-  ASSERT_TRUE(c.getFilter<message_filters::PassThrough<Msg>>(0));
-  ASSERT_FALSE(c.getFilter<message_filters::PassThrough<Msg>>(1));
+  ASSERT_TRUE(c.getFilter<PassThrough<Msg> >(0));
+  ASSERT_FALSE(c.getFilter<PassThrough<Msg> >(1));
 }
 
 TEST(Chain, retrieveFilterThroughBaseClass)
 {
-  message_filters::Chain<Msg> c;
-  message_filters::ChainBase * cb = &c;
+  Chain<Msg> c;
+  ChainBase* cb = &c;
 
-  ASSERT_FALSE(cb->getFilter<message_filters::PassThrough<Msg>>(0));
+  ASSERT_FALSE(cb->getFilter<PassThrough<Msg> >(0));
 
-  c.addFilter(std::make_shared<message_filters::PassThrough<Msg>>());
+  c.addFilter(std::make_shared<PassThrough<Msg> >());
 
-  ASSERT_TRUE(cb->getFilter<message_filters::PassThrough<Msg>>(0));
-  ASSERT_FALSE(cb->getFilter<message_filters::PassThrough<Msg>>(1));
+  ASSERT_TRUE(cb->getFilter<PassThrough<Msg> >(0));
+  ASSERT_FALSE(cb->getFilter<PassThrough<Msg> >(1));
 }
 
-struct PTDerived : public message_filters::PassThrough<Msg>
+struct PTDerived : public PassThrough<Msg>
 {
+
 };
 
 TEST(Chain, retrieveBaseClass)
 {
-  message_filters::Chain<Msg> c;
+  Chain<Msg> c;
   c.addFilter(std::make_shared<PTDerived>());
-  ASSERT_TRUE(c.getFilter<message_filters::PassThrough<Msg>>(0));
+  ASSERT_TRUE(c.getFilter<PassThrough<Msg> >(0));
   ASSERT_TRUE(c.getFilter<PTDerived>(0));
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
   rclcpp::init(argc, argv);
-  auto ret = RUN_ALL_TESTS();
-  rclcpp::shutdown();
-  return ret;
+  return RUN_ALL_TESTS();
 }
+
+
